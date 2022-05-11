2d612c5f73160a0b14266a8d1ab06257d67186a0
nobody-r <coparei7@gmail.com>
2022-05-11 18:10:49 +0200

    test

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = "key"
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+@app.route('/confronta', methods=["POST", "GET"])
+def confronta():
+    consumo=request.form['consumo_f1']
+    print(str(consumo))
+    return render_template("confronto.html")
 
 if __name__ == '__main__':
     app.run()
