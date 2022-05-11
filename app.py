b8b4d26cefd81d353839437424c6828f74bde117
nobody-r <coparei7@gmail.com>
2022-05-11 19:51:50 +0200

    test

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.secret_key = "key"
@@ -9,8 +9,24 @@ def index():
 
 @app.route('/confronta', methods=["POST", "GET"])
 def confronta():
-    consumo=request.form['consumo_f1']
-    print(str(consumo))
+    consumof1 = round(float(request.form['consumo_f1']) * (1 + float(request.form['perdite_rete'])/100), 1)
+    consumof2 = round(float(request.form['consumo_f2']) * (1 + float(request.form['perdite_rete'])/100), 1)
+    consumof3 = round(float(request.form['consumo_f3']) * (1 + float(request.form['perdite_rete'])/100), 1)
+    costo_totale_attuale_f1 = round(float(request.form['costo_attuale_f1'])*consumof1,1)
+    costo_totale_attuale_f2 = round(float(request.form['costo_attuale_f2'])*consumof2,1)
+    costo_totale_attuale_f3 = round(float(request.form['costo_attuale_f3'])*consumof3,1)
+    costo_totale_fareconsulenza_f1 = round(float(request.form['costo_fareconsulenza_f1'])*consumof1,1)
+    costo_totale_fareconsulenza_f2 = round(float(request.form['costo_fareconsulenza_f2'])*consumof2,1)
+    costo_totale_fareconsulenza_f3 = round(float(request.form['costo_fareconsulenza_f3'])*consumof3,1)
+    ripsarmio_euro_f1 = round(costo_totale_attuale_f1-costo_totale_fareconsulenza_f1, 1)
+    ripsarmio_euro_f2 = round(costo_totale_attuale_f2-costo_totale_fareconsulenza_f2, 1)
+    ripsarmio_euro_f3 = round(costo_totale_attuale_f3-costo_totale_fareconsulenza_f3, 1)
+    risparmio_euro_totale=ripsarmio_euro_f3+ripsarmio_euro_f2+ripsarmio_euro_f1
+    ripsarmio_percentuale_f1 = (costo_totale_fareconsulenza_f1 * 100 / costo_totale_attuale_f1)
+    ripsarmio_percentuale_f2 = (costo_totale_fareconsulenza_f2 * 100 / costo_totale_attuale_f2)
+    ripsarmio_percentuale_f3 = (costo_totale_fareconsulenza_f3 * 100 / costo_totale_attuale_f3)
+    risparmio_percentuale_totale = ripsarmio_percentuale_f1+ripsarmio_percentuale_f2+ripsarmio_percentuale_f3
+
     return render_template("confronto.html")
 
 if __name__ == '__main__':
