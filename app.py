a730fcf9fb495dfdfc6aa5f680dc5705ac1daff0
nobody-r <coparei7@gmail.com>
2022-05-12 12:50:53 +0200

    adding pie chart

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 app = Flask(__name__)
 app.secret_key = "key"
 
+#FUNCTIONS
 def grafico_a_barre(cattuale1, cattuale2, cattuale3, cfare1, cfare2, cfare3):
     labels = ['F1', 'F2', 'F3']
     Attuale = [cattuale1, cattuale2, cattuale3]
@@ -24,6 +25,29 @@ def grafico_a_barre(cattuale1, cattuale2, cattuale3, cfare1, cfare2, cfare3):
     fig.tight_layout()
     plt.savefig('static/graficobarre.png')
 
+def grafico_a_torta(ctot_attuale, ctot_fare):
+    fig, ax = plt.subplots(figsize=(8, 3), subplot_kw=dict(aspect="equal"))
+    recipe = [(str(ctot_attuale)+"€ Costo attuale fornitore"),
+              (str(ctot_fare)+"€ Costo Fareconsulenza"),
+              (str(round((ctot_attuale-ctot_fare), 1))+"€ RISPARMIO")]
+    data = [ctot_attuale, ctot_fare, (ctot_attuale-ctot_fare)]
+    colors = ['red', 'orange', 'green']
+    wedges, texts = ax.pie(data, wedgeprops=dict(width=0.5), startangle=-40, colors=colors)
+    bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
+    kw = dict(arrowprops=dict(arrowstyle="-"),bbox=bbox_props, zorder=0, va="center")
+
+    for i, p in enumerate(wedges):
+        ang = (p.theta2 - p.theta1) / 2. + p.theta1
+        y = np.sin(np.deg2rad(ang))
+        x = np.cos(np.deg2rad(ang))
+        horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+        connectionstyle = "angle,angleA=0,angleB={}".format(ang)
+        kw["arrowprops"].update({"connectionstyle": connectionstyle})
+        ax.annotate(recipe[i], xy=(x, y), xytext=(1.35 * np.sign(x), 1.4 * y),
+                    horizontalalignment=horizontalalignment, **kw)
+    plt.savefig('static/graficotorta.png')
+
+#FLASK APP
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -52,6 +76,7 @@ def confronta():
     grafico_a_barre(costo_totale_attuale_f1, costo_totale_attuale_f2, costo_totale_attuale_f3, costo_totale_fareconsulenza_f1,
                     costo_totale_fareconsulenza_f2, costo_totale_fareconsulenza_f3)
 
+    grafico_a_torta(costo_totale_attuale, costo_totale_fareconsulenza)
 
     return render_template("confronto.html",
                            risparmio_euro_f1=risparmio_euro_f1,
