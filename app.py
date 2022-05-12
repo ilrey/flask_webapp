9c280f140b06e4186ef2e216045a2ef667398193
nobody-r <coparei7@gmail.com>
2022-05-12 17:35:25 +0200

    adjustments + pdf creation

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 #LIBRARIES AND FLASK FRAMEWORK
 from flask import Flask, render_template, request, redirect, send_file
 import numpy as np
-import time
 import matplotlib.pyplot as plt
 from fpdf import FPDF, HTMLMixin
 
